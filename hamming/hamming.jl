156dd40b58adbe53d97aa9ce1c6e6ba363f24a83
Michał Kucharczyk <61332345+Rigante-pl@users.noreply.github.com>
2021-04-13 21:59:09 +0200

    Delete hamming directory

--- hamming/hamming.jl
+++
@@ -1,5 +0,0 @@
-"Calculate the Hamming Distance between two DNA strands"
-function distance(a, b)
-    length(a)==length(b) || throw(ArgumentError("diffrend strands length"))
-    sum(collect(a).!=collect(b))
-end
